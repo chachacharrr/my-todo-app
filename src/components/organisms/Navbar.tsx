760dcb48248059f97000da3bf9f41e03d9f6a84b
chachacharrr <167711470+chachacharrr@users.noreply.github.com>
2025-01-10 06:36:53 +0900

    ✨ navbarの修正

--- src/components/organisms/Navbar.tsx
+++ src/components/organisms/Navbar.tsx
@@ -5,14 +5,16 @@ export const Navbar = () => {
   return (
     <>
       <div className="bg-blue-300 flex h-10 items-center justify-between text-gray-800">
-        <div className="ml-4">
+        <div className="flex w-40 ml-10 ">
           <Link href="/">Todo App</Link>
         </div>
-        <Link href="https://github.com/chachacharrr/my-todo-app">
-          <div className="mr-4">
-            <FaGithub />
-          </div>
-        </Link>
+        <div className="mr-10">
+          <Link href="https://github.com/chachacharrr/my-todo-app">
+            <div className="">
+              <FaGithub />
+            </div>
+          </Link>
+        </div>
       </div>
     </>
   );
