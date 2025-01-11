f561171e036ee23e7088c4438e674e1180ebe06f
chachacharrr <167711470+chachacharrr@users.noreply.github.com>
2025-01-11 17:56:09 +0900

    ✨ googleのログイン機能を追加

--- src/components/pages/UserLoginPage.tsx
+++ src/components/pages/UserLoginPage.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+"use client";
+
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { auth } from "../../../firebase.config";
 
 const UserLoginPage = () => {
-  return <div>ログインページです</div>;
+  const handleGoogleSignIn = async () => {
+    try {
+      const provider = new GoogleAuthProvider();
+      const result = await signInWithPopup(auth, provider);
+      // Handle successful sign-in (e.g., redirect to a protected page)
+      console.log(result.user); // Access user information
+    } catch (error) {
+      // Handle sign-in errors
+      console.error(error);
+    }
+  };
+
+  return <button onClick={handleGoogleSignIn}>Sign in with Google</button>;
 };
 
 export default UserLoginPage;
