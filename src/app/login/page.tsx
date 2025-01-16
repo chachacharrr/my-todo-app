9ad081b03ef989221d47a9a9451f4bed05a663f8
chachacharrr <167711470+chachacharrr@users.noreply.github.com>
2025-01-17 04:59:14 +0900

    firebaseのログイン機能を追加

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,9 +2,11 @@ import UserLoginPage from "@/components/pages/UserLoginPage";
 
 const LoginPage = () => {
   return (
-    <>
-      <UserLoginPage />
-    </>
+    <div className="flex  mt-16 items-center justify-center">
+      <div className=" border-white border p-3 rounded-md bg-gray-500 hover:bg-gray-700">
+        <UserLoginPage />
+      </div>
+    </div>
   );
 };
 
--- src/components/pages/UserLoginPage.tsx
+++ src/components/pages/UserLoginPage.tsx
@@ -2,17 +2,21 @@
 
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import { auth } from "../../../firebase.config";
+import { useRouter } from "next/navigation"; // useRouterを追加
 
 const UserLoginPage = () => {
+  const router = useRouter(); // useRouterを初期化
+
   const handleGoogleSignIn = async () => {
     try {
       const provider = new GoogleAuthProvider();
       const result = await signInWithPopup(auth, provider);
       // Handle successful sign-in (e.g., redirect to a protected page)
-      console.log(result.user); // Access user information
+      console.log(`ログイン成功：${result.user}`); // Access user information
+      router.push("/"); // ログイン後にホームページにリダイレクト
     } catch (error) {
       // Handle sign-in errors
-      console.error(error);
+      console.error(`ログイン失敗：${error}`);
     }
   };
 
