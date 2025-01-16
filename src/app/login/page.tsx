3537e5179704a80f40f3ea99027e58c41e90150e
chachacharrr <167711470+chachacharrr@users.noreply.github.com>
2025-01-17 05:30:59 +0900

    ✨ ゲストログインの追加

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,10 +2,8 @@ import UserLoginPage from "@/components/pages/UserLoginPage";
 
 const LoginPage = () => {
   return (
-    <div className="flex  mt-16 items-center justify-center">
-      <div className=" border-white border p-3 rounded-md bg-gray-500 hover:bg-gray-700">
-        <UserLoginPage />
-      </div>
+    <div>
+      <UserLoginPage />
     </div>
   );
 };
--- src/components/pages/UserLoginPage.tsx
+++ src/components/pages/UserLoginPage.tsx
@@ -1,6 +1,10 @@
 "use client";
 
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  signInWithPopup,
+  GoogleAuthProvider,
+  signInAnonymously,
+} from "firebase/auth";
 import { auth } from "../../../firebase.config";
 import { useRouter } from "next/navigation"; // useRouterを追加
 
@@ -20,7 +24,33 @@ const UserLoginPage = () => {
     }
   };
 
-  return <button onClick={handleGoogleSignIn}>Sign in with Google</button>;
+  const handleGuestSignIn = async () => {
+    try {
+      const result = await signInAnonymously(auth);
+      console.log(`ゲストログイン成功：${result.user}`);
+      router.push("/");
+    } catch (error) {
+      console.error(`ゲストログイン失敗：${error}`);
+    }
+  };
+
+  return (
+    <div className="flex-col border rounded-md border-white w-2/3 justify-self-center mt-40">
+      <button
+        className="flex m-auto pt-5 border-b-2"
+        onClick={handleGoogleSignIn}
+      >
+        Sign in with Google
+      </button>
+
+      <button
+        className="flex m-auto mb-5 pt-5 border-b-2"
+        onClick={handleGuestSignIn}
+      >
+        ゲストログイン
+      </button>
+    </div>
+  );
 };
 
 export default UserLoginPage;
